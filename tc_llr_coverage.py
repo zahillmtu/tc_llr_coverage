1325f0c1fa765c4cbcd46497a2ed57eaa47c6a83
Zachary Hill <zachary13hill@yahoo.com>
2016-08-01 11:53:54 -0400

    Finishing touches

--- tc_llr_coverage.py
+++ tc_llr_coverage.py
@@ -1,4 +1,25 @@
-"""Script to determine the coverage of modified LLRs in the Test Cases"""
+"""Script to determine the coverage of modified LLRs in the Test Cases
+
+This script will travese the specified file directory, into all sub-directories,
+to determine if all of the LLRs specified are covered by the test cases in
+the directory. If an LLR is not listed within any of the files, the name of the
+LLR will be printed out in the 'coverage_results.log' log file.
+
+NOTE:
+    The LLRs you would like to check for need to be placed in a '.txt' file
+    or '.csv'. Each LLR MUST be on a separate new line. The name of the file
+    is not important.
+
+TO RUN:
+    Run the script using Python 2.7 and a dialog box will appear, select
+    the root directory. A second dialog box will appear asking you to
+    select a file, this is where you will select the LLR file that is
+    specified above in the 'NOTE' section.
+
+OUTPUT:
+    The output of the script will be directed to 'coverage_results.log'.
+
+"""
 
 __author__ = "Zachary Hill"
 __email__ = "zachary.hill@psware.com"
@@ -9,6 +30,7 @@ import sys
 
 from Tkinter import Tk
 from tkFileDialog import askdirectory
+from tkFileDialog import askopenfilename
 
 RES_FILE = './coverage_results.log'
 MODIFIED_TC_FILE = './modified_tc_file.csv'
@@ -63,13 +85,13 @@ def find_requirements(fileName, fullFileName, cases_covered):
 
     cases_covered.extend(requires)
 
-def gather_modified(modified):
+def gather_modified(modified, modified_file):
     """Method to retrieve the modified requirements from
     a .csv file
     """
 
     # Read in data
-    with open(MODIFIED_TC_FILE, 'r') as f:
+    with open(modified_file, 'r') as f:
         data = f.read().splitlines(True)
 
     modified.extend(data)
@@ -88,6 +110,10 @@ def main():
     rootDir = askdirectory() # show an "Open" dialog box and return the path to the selected file
     print('Head of tree travesal selected %s' % rootDir)
 
+    Tk().withdraw()
+    modified_file = askopenfilename()
+    print('File of LLRs to test coverage against %s' % modified_file)
+
     # need variables to store data
     modified = []
     cases_covered = []
@@ -103,7 +129,7 @@ def main():
                 find_requirements(file, os.path.join(dirName, file), cases_covered)
 
     # get modified data
-    gather_modified(modified)
+    gather_modified(modified, modified_file)
 
     not_found = 0
     print('')
